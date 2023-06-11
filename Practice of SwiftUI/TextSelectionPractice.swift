f0ccdde5ea613ce2e321f654d323a65736342d3b
shenqing <shenqin1@msu.edu>
2023-06-11 11:36:40 -0400

    Text selection in swiftUI

---
+++ Practice of SwiftUI/TextSelectionPractice.swift
@@ -0,0 +1,21 @@
+//
+//  TextSelectionPractice.swift
+//  Practice of SwiftUI
+//
+//  Created by 沈清昊 on 6/11/23.
+//
+
+import SwiftUI
+
+struct TextSelectionPractice: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+struct TextSelectionPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionPractice()
+    }
+}
