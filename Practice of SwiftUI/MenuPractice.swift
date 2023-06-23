1663a600a19667770c8989b7e994c1d7aacca299
shenqing <shenqin1@msu.edu>
2023-06-23 12:56:56 -0400

    Menu practice in swiftUI

---
+++ Practice of SwiftUI/MenuPractice.swift
@@ -0,0 +1,36 @@
+//
+//  MenuPractice.swift
+//  Practice of SwiftUI
+//
+//  Created by 沈清昊 on 6/23/23.
+//
+
+import SwiftUI
+
+struct MenuPractice: View {
+    var body: some View {
+        Menu("Click me") {
+            Button("One") {
+                
+            }
+            
+            Button("Two") {
+                
+            }
+            
+            Button("Three") {
+                
+            }
+            
+            Button("Four") {
+                
+            }
+        }
+    }
+}
+
+struct MenuPractice_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuPractice()
+    }
+}
